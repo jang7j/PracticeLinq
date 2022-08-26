b5fa49bd7b81a7dc2ab1193a2d11c04f81d9733c
Yun Jang <jang7j@yahoo.com>
2022-08-26 11:14:24 -0500

    initial commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,5 @@
+﻿List<string> videoGames = new List<string>() { "Starcraft", "Warcraft", "MarioParty", "Zelda", "Sonic_The_Hedgehog" };
+
+videoGames.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x));
+
+
